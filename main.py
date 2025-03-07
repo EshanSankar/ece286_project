7079103dcc53bb08438b5f28d7443ea2b04a1f0b
EshanSankar <eshansankar12@gmail.com>
2025-03-06 21:28:20 -0500

    all functionality is there

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # TODO:
 # -centering the buttons and text properly
-# -adding a tutorial gif
-# -building the experiment
+# -adding the actual tutorial gif and fixing the lag
+# -adding the actual experiment images
 
 import tkinter as tk
 from PIL import Image, ImageTk
 import cv2
+import time
 
 class App:
     def __init__(self, master):
@@ -14,12 +15,14 @@ class App:
         self.HEADER_FONT = "Arial 32 bold"
         self.BODY_FONT = "Arial 14"
         self.isDrawing = False
-        self.isModifying, self.isModifyingLeft, self.isModifyingRight = False, False, False
+        self.isModifying = False
         self.fullRectangle = False
+        self.edge_being_modified = None
         self.startX, self.startY, self.endX, self.endY = 0, 0, 0, 0
-        self.clickOffset = 0
-        self.annotatorWindowName = "Annotator"
+        self.clickOffset, self.clickOffsetL, self.clickOffsetT, self.clickOffsetR, self.clickOffsetB = 0, 0, 0, 0, 0
         self.temp_img = None
+        self.beginTime = 0
+        self.pracTime = 0
         self.home()
     
     def home(self):
@@ -32,7 +35,7 @@ class App:
         self.text_home = tk.Label(self.frame_home, text="Welcome to the data annotator!", font=self.HEADER_FONT)
         self.text_home.grid(row=0,column=0)
         self.text_body = tk.Label(self.frame_home,
-                                  text="Thank you for participating in our experiment! \n Here, you will perform bounding-box data annotation on images of stop signs. \n When you click \'Continue\', you will be given a brief tutorial.",
+                                  text="Thank you for participating in our experiment! \n Here, you will perform bounding-box data annotation on images of stop signs. \n When you click 'Continue', you will be given a brief tutorial.",
                                   font=self.BODY_FONT)
         self.text_body.grid(row=1, column=0, sticky="n")
         self.button_begin = tk.Button(self.frame_home, text="Continue", width=25, height=5, command=self.guide)
@@ -50,21 +53,21 @@ class App:
                                    font=self.BODY_FONT)
         self.text1_guide.grid(row=0, column=0, sticky="s")
         self.text2_guide = tk.Label(self.frame_guide,
-                                   text="For each image, you will be given a small default box, the corners of which you can freely drag such that the box captures the stop sign. \n Press [SPACEBAR] as soon as you are satisfied with your drawing! \n Here is an example of a good bounding box being drawn:",
+                                   text="For each image, you will be given a small default box, the corners of which you can freely drag such that the box captures the stop sign. \n Press 'Finish' as soon as you are satisfied with your drawing! \n Here is an example of a good bounding box being drawn:",
                                    font=self.BODY_FONT)
         self.text2_guide.grid(row=1, column=0, sticky="s")
         # gif configuration -- NEED TO ADD ANNOTATION GIF
-        # file = "annotation.gif" # NEED TO ADD THIS
-        # gif = Image.open(file)
-        # self.frames = gif.n_frames
-        # self.photoimage_objects = []
-        # for i in range(self.frames):
-        #     obj = tk.PhotoImage(file = file, format = f"gif -index {i}")
-        #     self.photoimage_objects.append(obj)
-        # self.gif_guide = tk.Label(self.frame_guide)
-        # self.gif_guide.grid(row=2, column=0)
-        # self.current_frame = 0
-        # self.animation()
+        file = "annotation.gif" # NEED TO ADD THIS
+        gif = Image.open(file)
+        self.frames = gif.n_frames
+        self.photoimage_objects = []
+        for i in range(self.frames):
+            obj = tk.PhotoImage(file = file, format = f"gif -index {i}")
+            self.photoimage_objects.append(obj)
+        self.gif_guide = tk.Label(self.frame_guide)
+        self.gif_guide.grid(row=2, column=0)
+        self.current_frame = 0
+        self.animation()
         # end gif configuration
         self.button_trial = tk.Button(self.frame_guide, text="Do an Example Annotation", width=25, height=5, command=self.trial)
         self.button_trial.grid(row=3, column=0)
@@ -72,26 +75,30 @@ class App:
     def trial(self):
         for i in self.master.winfo_children():
             i.destroy()
+        self.fullRectangle = False
         self.frame_trial = tk.Frame(self.master, width=1920, height=1080)
         self.frame_trial.pack(expand=True, fill="both")
-        self.button_end = tk.Button(self.frame_trial, text="Done", width=25, height=5, command=self.begin_screen)
+        self.button_end = tk.Button(self.frame_trial, text="Finish", width=25, height=5, command=self.begin_screen)
         self.button_end.pack()
         self.image_cv2 = cv2.imread("Untitled.png", cv2.IMREAD_COLOR)
-        cv2.namedWindow(self.annotatorWindowName)
-        cv2.setMouseCallback(self.annotatorWindowName, self.drawRectangle, self.image_cv2)
-        cv2.imshow(self.annotatorWindowName, self.image_cv2)
+        cv2.namedWindow("Annotator")
+        cv2.setMouseCallback("Annotator", self.drawRectangle, self.image_cv2)
+        cv2.imshow("Annotator", self.image_cv2)
+        self.beginTime = time.time()
         
 
     def begin_screen(self):
+        self.pracTime = time.time() - self.beginTime 
         cv2.destroyAllWindows()
         for i in self.master.winfo_children():
             i.destroy()
+        #print(f"{self.startX}, {self.startY}, {self.endX}, {self.endY}")
         self.frame_begin_screen = tk.Frame(self.master, width=1920, height=1080)
         self.frame_begin_screen.columnconfigure(0, minsize=350)
         self.frame_begin_screen.rowconfigure([0, 1, 2], minsize=200)
         self.frame_begin_screen.pack(expand=True, fill="both")
         self.text_begin = tk.Label(self.frame_begin_screen,
-                                   text="You are about to begin the actual data annotation experiment. \n \'Begin Experiment\' will start a timer and take you to the images. \n Remember to press [SPACEBAR] after finishing each box. \n After 10 annotations, the program will finish.",
+                                   text=f"Time: {self.pracTime}. IoU: {self.calculateIoU([19, 65, 342, 393])}. \n You are about to begin the actual data annotation experiment. \n 'Begin Experiment' will start a timer and take you to the images. \n Remember to press 'Finish' after finishing each box. \n After 10 annotations, the program will finish.",
                                    font=self.HEADER_FONT)
         self.text_begin.grid(row=0, column=0, sticky="s")
         self.button_begin_experiment = tk.Button(self.frame_begin_screen, text="Begin Experiment", width=25, height=5, command=self.begin_experiment)
@@ -102,11 +109,11 @@ class App:
     def begin_experiment(self):
         root.destroy()
     
-    #def animation(self): # for gif
-    #    if self.frames:
-    #        self.gif_guide.config(image=self.photoimage_objects[self.current_frame])
-    #        self.current_frame = self.current_frame + 1 if self.current_frame != self.frames else 0
-    #        self.frame_guide.after(50, self.animation)
+    def animation(self): # for gif
+       if self.frames:
+           self.gif_guide.config(image=self.photoimage_objects[self.current_frame])
+           self.current_frame = self.current_frame + 1 if self.current_frame != self.frames else 0
+           self.frame_guide.after(50, self.animation)
 
     def drawRectangle(self, event, x, y, flags, image):
         img = image
@@ -116,7 +123,7 @@ class App:
         elif event == cv2.EVENT_MOUSEMOVE and self.isDrawing:
             self.temp_img = img.copy()
             cv2.rectangle(self.temp_img, (self.startX, self.startY), (x, y), (255, 0, 0), 3)
-            cv2.imshow(self.annotatorWindowName, self.temp_img)
+            cv2.imshow("Annotator", self.temp_img)
         elif event == cv2.EVENT_LBUTTONUP and self.isDrawing and not self.fullRectangle:
             self.isDrawing = False
             self.fullRectangle = True
@@ -127,22 +134,67 @@ class App:
             if self.endY < self.startY:
                 self.endY, self.startY = self.startY, self.endY
             img = self.temp_img
-            cv2.imshow(self.annotatorWindowName, img)
-        elif event == cv2.EVENT_LBUTTONDOWN and not self.isDrawing and self.fullRectangle and not self.isModifying:
-            self.clickOffset = x - self.startX
-            if abs(self.clickOffset) <= 50 and y > self.startY and y < self.endY:
-                self.isModifyingLeft = True
-        elif event == cv2.EVENT_MOUSEMOVE and self.isModifyingLeft:
+            cv2.imshow("Annotator", img)
+        
+        elif event == cv2.EVENT_LBUTTONDOWN and self.fullRectangle and not self.isModifying:
+            self.clickOffsetL = x - self.startX
+            self.clickOffsetT = y - self.startY
+            self.clickOffsetR = x - self.endX
+            self.clickOffsetB = y - self.endY
+
+            self.clickOffsetLR = self.clickOffsetL if abs(self.clickOffsetL) < abs(self.clickOffsetR) else self.clickOffsetR
+            self.lr_being_modified = 'l' if abs(self.clickOffsetL) < abs(self.clickOffsetR) else 'r'
+            self.clickOffsetTB = self.clickOffsetT if abs(self.clickOffsetT) < abs(self.clickOffsetB) else self.clickOffsetB
+            self.tb_being_modified = 't' if abs(self.clickOffsetT) < abs(self.clickOffsetB) else 'b'
+            self.edge_being_modified = self.lr_being_modified if abs(self.clickOffsetLR) < abs(self.clickOffsetTB) else self.tb_being_modified
+            self.clickOffset = self.clickOffsetLR if abs(self.clickOffsetLR) < abs(self.clickOffsetTB) else self.clickOffsetTB
+            if abs(self.clickOffset) > 15:
+                return
+
+            self.clickOffsetL = 0 if self.edge_being_modified != 'l' else self.clickOffsetL
+            self.clickOffsetT = 0 if self.edge_being_modified != 't' else self.clickOffsetT
+            self.clickOffsetR = 0 if self.edge_being_modified != 'r' else self.clickOffsetR
+            self.clickOffsetB = 0 if self.edge_being_modified != 'b' else self.clickOffsetB
+            self.lr_being_modified, self.tb_being_modified = None, None
+            self.isModifying = True
+            print(self.edge_being_modified)
+        elif event == cv2.EVENT_MOUSEMOVE and self.isModifying:
             self.temp_img = img.copy()
-            cv2.rectangle(self.temp_img, (x - self.clickOffset, self.startY), (self.endX, self.endY), (255, 0, 0), 3)
-            cv2.imshow(self.annotatorWindowName, self.temp_img)
-        elif event == cv2.EVENT_LBUTTONUP and self.isModifyingLeft:
-            self.isModifyingLeft = False
-            self.startX = x
+            L = x if self.edge_being_modified == 'l' else self.startX
+            T = y if self.edge_being_modified == 't' else self.startY
+            R = x if self.edge_being_modified == 'r' else self.endX
+            B = y if self.edge_being_modified == 'b' else self.endY
+            cv2.rectangle(self.temp_img, (L - self.clickOffsetL, T - self.clickOffsetT), (R - self.clickOffsetR, B - self.clickOffsetB), (255, 0, 0), 3)
+            cv2.imshow("Annotator", self.temp_img)
+        elif event == cv2.EVENT_LBUTTONUP and self.isModifying:
+            self.isModifying = False
+            self.clickOffset, self.clickOffsetL, self.clickOffsetT, self.clickOffsetR, self.clickOffsetB = 0, 0, 0, 0, 0
+            
+            self.startX = x if self.edge_being_modified == 'l' else self.startX
+            self.startY = y if self.edge_being_modified == 't' else self.startY
+            self.endX = x if self.edge_being_modified == 'r' else self.endX
+            self.endY = y if self.edge_being_modified == 'b' else self.endY
+            self.edge_being_modified = None
             if self.endX < self.startX:
                 self.endX, self.startX = self.startX, self.endX
+            if self.endY < self.startY:
+                self.endY, self.startY = self.startY, self.endY
             img = self.temp_img
-            cv2.imshow(self.annotatorWindowName, img)
+            cv2.imshow("Annotator", img)
+    
+    def calculateIoU(self, ground_truth): #ground truth is a list: [GTstartX, GTstartY, GTendX, GTendY]
+        A_measured = (self.endX - self.startX)*(self.endY - self.startY)
+        A_ground_truth = (ground_truth[2] - ground_truth[0])*(ground_truth[3] - ground_truth[1])
+        l_union = max(self.endX, ground_truth[2]) - min(self.startX, ground_truth[0])
+        h_union = max(self.endY, ground_truth[3]) - min(self.startY, ground_truth[1])
+        A_union = l_union * h_union
+        l_overlap = max(self.startX, ground_truth[0]) - min(self.endX, ground_truth[2]) if ((self.startX > ground_truth[0] and self.startX < ground_truth[2]) or (self.endX > ground_truth[0] and self.endX < ground_truth[2])) else 0
+        h_overlap = max(self.startY, ground_truth[1]) - min(self.endY, ground_truth[3]) if ((self.startY > ground_truth[1] and self.startY < ground_truth[3]) or (self.endY > ground_truth[1] and self.endY < ground_truth[3])) else 0
+        A_overlap = l_overlap * h_overlap
+        A_union = A_measured + A_ground_truth - A_overlap
+        IoU = A_overlap / A_union
+        return IoU
+
 
 
             
